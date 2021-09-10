d4e336c877908aa6064f024869098bd4599b1306
Jaehun Park <pjaehun4122@gmail.com>
2021-09-10 00:54:48 -0700

    is this ok for commit

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+
+
+
+
+
+
+
+export default function Character(props){
+    
+const {characters}=props;
+console.log(props.characters);
+
+
+    return(
+        <div>
+            {characters.map(character=>{
+                return <div>{character.name}</div>
+            })}
+        </div>
+
+    )
+
+
+}
